b7e92ca3e8cd142ded3cacc05a36970ba14536cd
M Nouman Saeed <contact.miang15@gmail.com>
2022-01-26 19:37:50 +0500

    fixes

--- App/Screens/Auth/ForgotPassword.js
+++ App/Screens/Auth/ForgotPassword.js
@@ -1,15 +1,39 @@
-import React, { useState } from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {Alert, ScrollView, StyleSheet, Text, View} from 'react-native';
 import Button from '../../components/Button';
 import CustomInput from '../../components/CustomInput';
 import Header from '../../components/Header';
 import Icons from '../../constants/Icons';
+import { forgotPassword } from '../../Services/Apis';
 import Theme from '../../utils/Theme';
 
 const ForgotPassword = ({navigation}) => {
-  const [email, setEmail] = useState('')
-  const [emailError, setEmailError] = useState('')
-  
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const handleForgotPassword = () => {
+    setEmailError('');
+
+    if(email == ''){
+      return setEmailError("Email is Required")
+    }
+    else if(!email.match(emailRegex)){
+      return setEmailError("Enter valid Email")
+    } else {
+      forgotPassword(email).then((res) => {
+        console.log("RES: ",res)
+        navigation.navigate("ForgotPin")
+      }).catch((e) => {
+        console.log("ERROR: ",e);
+        Alert.alert("Something went wrong")
+      })
+    }
+
+  }
+
   return (
     <View style={styles.container}>
       <Header onPress={() => navigation.goBack()} />
@@ -20,9 +44,17 @@ const ForgotPassword = ({navigation}) => {
             Please enter your email below to receive your password reset code
           </Text>
           <Text style={styles.label}>Enter the email</Text>
-          <CustomInput placeholder="Your email" LeftIcons={Icons.Email} />
+          <CustomInput
+            placeholder="Your email"
+            LeftIcons={Icons.Email}
+            value={email}
+            onChangeText={setEmail}
+          />
+          {emailError ? (
+            <Text style={styles.errorMsg}>{emailError}</Text>
+          ) : null}
           <Button
-            onPress={() => navigation.navigate('ForgotPin')}
+            onPress={handleForgotPassword}
             title="Send Code"
             top="10%"
           />
@@ -59,5 +91,9 @@ const styles = StyleSheet.create({
     color: Theme.white,
     fontSize: 15,
     marginTop: '15%',
+  },
+  errorMsg: {
+    color: Theme.red,
+    fontSize: 13,
   },
 });
